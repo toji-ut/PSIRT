b3727f37e957ade0726abaeedd56db5ca98a22db
toji-ut <107822013+toji-ut@users.noreply.github.com>
2023-12-12 17:01:30 -0500

    made the whole frontend, just need php functionality and database

--- frontend/authentication/login.php
+++ frontend/authentication/login.php
@@ -3,16 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
-    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="../globalStyles/styles.css">
 </head>
 <body>
     <div class="login-container">
         <h2>Login</h2>
         <form action="login.php" method="POST">
             <label for="username">Username:</label>
-            <input type="text" id="username" name="username" required><br><br>
+            <input type="text" id="username" name="username" required>
             <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required><br><br>
+            <input type="password" id="password" name="password" required>
             <input type="submit" value="Login">
         </form>
     </div>
