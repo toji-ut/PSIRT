4b2b3032839512d7a38ecd035185a2c36a853ddf
Onkar Dhillon <onkardhillon73@gmail.com>
2023-12-13 21:31:55 -0500

    Sitter Done.

--- frontend/authentication/sitter/sitter.php
+++ frontend/authentication/sitter/sitter.php
@@ -23,28 +23,24 @@
 
         $conn = new mysqli($servername, $username, $password, $dbname);
 
-
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
-
 
         // Start the session
         session_start();
 
         // Set the UserID in the session
         $currentUserID = $_SESSION['UserID'];
+
         // Retrieve data from the database
         $sql = "SELECT OrderID, ServiceState, ClientID, OrderDate, DueDate FROM Orders WHERE `SitterID` = $currentUserID AND `ServiceState`= 'assigned'";
-
         $result = $conn->query($sql);
 
         if ($result->num_rows > 0) {
             echo '<table>';
-            echo '<tr><th>OrderID</th><th>Service State</th><th>Client ID</th><th>Order Date</th><th>Due Date</th></tr>';
+            echo '<tr><th>OrderID</th><th>Service State</th><th>Client ID</th><th>Order Date</th><th>Due Date</th><th>Action</th></tr>';
 
-            
-            // Loop through the fetched data and display it in table rows
             while ($row = $result->fetch_assoc()) {
                 echo "<tr>";
                 echo "<td>" . $row['OrderID'] . "</td>";
@@ -52,10 +48,11 @@
                 echo "<td>" . $row['ClientID'] . "</td>";
                 echo "<td>" . $row['OrderDate'] . "</td>";
                 echo "<td>" . $row['DueDate'] . "</td>";
-                echo "<form action='' method='post'>";
-                echo "<input type='hidden' name='orderID' value='" . $row['OrderID'] . "'>";
-                echo "<input type='submit' value='Complete'>";
-                echo "</form>";
+                echo "<td>";
+                echo '<form action="" method="post">';
+                echo '<input type="hidden" name="orderID" value="' . $row['OrderID'] . '">';
+                echo '<input type="submit" name="completeOrder" value="Complete">';
+                echo '</form>';
                 echo "</td>";
                 echo "</tr>";
             }
@@ -65,18 +62,18 @@
 
         if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['completeOrder'])) {
             $orderID = $_POST['orderID'];
-        
+
             // Update the status of the order from "Assigned" to "Completed"
             $updateQuery = "UPDATE Orders SET ServiceState = 'completed' WHERE OrderID = $orderID";
-        
+
             if ($conn->query($updateQuery) === TRUE) {
-                header("Location: sitterDashboard.php"); // Redirect to the sitter dashboard after completion
+                header("Location: ./sitter.php"); // Redirect to the sitter dashboard after completion
                 exit();
             } else {
                 echo "Error updating record: " . $conn->error;
             }
         }
-        
+
         $conn->close();
         ?>
 
@@ -98,21 +95,18 @@
 
         $conn = new mysqli($servername, $username, $password, $dbname);
 
-
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
 
         // Retrieve data from the database
         $sql = "SELECT OrderID, ServiceState, ClientID, OrderDate, DueDate FROM Orders WHERE `ServiceState`= 'completed'";
-
         $result = $conn->query($sql);
 
         if ($result->num_rows > 0) {
             echo '<table>';
             echo '<tr><th>OrderID</th><th>Service State</th><th>Client ID</th><th>Order Date</th><th>Due Date</th></tr>';
 
-            // Loop through the fetched data and display it in table rows
             while ($row = $result->fetch_assoc()) {
                 echo "<tr>";
                 echo "<td>" . $row['OrderID'] . "</td>";
